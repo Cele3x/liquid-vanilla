aea250d3107d8d665ea1a73ca7c95995becf6dc5
Jonathan Sasse <jonathan.sasse@outlook.de>
2024-09-07 12:46:54 +0200

    Update gunicorn command in run.sh
    Change the module path from `src.main` to `main` for application run.

--- backend/run.sh
+++ backend/run.sh
@@ -10,4 +10,4 @@ PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/src
 export PYTHONPATH
 
 # Run the application using gunicorn
-gunicorn src.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
